12e5224bb3bcdefbc2d968c3f627ff06f24ce687
김현룡 <hykim22@gmail.com>
2022-01-04 22:16:29 +0900

    Update SpringMain.java

--- src/test/java/com/pcwk/ehr/SpringMain.java
+++ src/test/java/com/pcwk/ehr/SpringMain.java
@@ -4,7 +4,7 @@ public class SpringMain {
 
 	public static void main(String[] args) {
 		System.out.println("================");
-		System.out.println("================");
+		System.out.println("Hello");
 		System.out.println("================");
 
 	}
