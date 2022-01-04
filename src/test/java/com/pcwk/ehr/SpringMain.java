ab5488b5dad7e07303928f58fdb79f6570bd328f
pcwk <hykim22@gmail.com>
2022-01-04 22:17:15 +0900

    co

--- src/test/java/com/pcwk/ehr/SpringMain.java
+++ src/test/java/com/pcwk/ehr/SpringMain.java
@@ -5,6 +5,7 @@ public class SpringMain {
 	public static void main(String[] args) {
 		System.out.println("================");
 		System.out.println("Hello");
+		System.out.println("Hello");
 		System.out.println("================");
 
 	}
